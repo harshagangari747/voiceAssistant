ebcd7abd33cf0633fff147503a91b06271d7e573
harshagangari747 <68506431+harshagangari747@users.noreply.github.com>
2021-05-13 01:08:06 +0530

    Update VoiceAssistant.py

--- VoiceAssistant.py
+++ VoiceAssistant.py
@@ -1,7 +1,5 @@
 import webbrowser
 import speech_recognition as sr 
-# import pyaudio
-# import wave,sys
 import os,time,random
 import datetime as dt 
 import requests,re,sys
@@ -9,22 +7,20 @@ from dictionary import telugudictionary as td
 import threading
 import rsaAlgo as rs
 from googletrans import Translator
-
 def userInput() :
     recAud = sr.Recognizer()
     with sr.Microphone() as source :
-        print("చెప్పండి...")
+        print(b'\xe0\xb0\x9a\xe0\xb1\x86\xe0\xb0\xaa\xe0\xb1\x8d\xe0\xb0\xaa\xe0\xb0\x82\xe0\xb0\xa1\xe0\xb0\xbf...'.decode('utf-8'))
         recAud.pause_threshold = 1.5
         audio = recAud.listen(source)
     try : 
-        print("వింటున్న...")
+        print(b'\xe0\xb0\xb5\xe0\xb0\xbf\xe0\xb0\x82\xe0\xb0\x9f\xe0\xb1\x81\xe0\xb0\xa8\xe0\xb1\x8d\xe0\xb0\xa8....'.decode('utf-8'))
         command = recAud.recognize_google(audio,language='te-IN')
-        print("జారీ చేసిన ఆదేశం : ", command)
+        print(b'\xe0\xb0\x9c\xe0\xb0\xbe\xe0\xb0\xb0\xe0\xb1\x80 \xe0\xb0\x9a\xe0\xb1\x87\xe0\xb0\xb8\xe0\xb0\xbf\xe0\xb0\xa8 \xe0\xb0\x86\xe0\xb0\xa6\xe0\xb1\x87\xe0\xb0\xb6\xe0\xb0\x82 :'.decode('utf-8'),command)
     except Exception as e :
-        print("మళ్ళీ చెప్పడానికి ప్రయత్నించండి...")
+        print(b'\xe0\xb0\xae\xe0\xb0\xb3\xe0\xb1\x8d\xe0\xb0\xb3\xe0\xb1\x80 \xe0\xb0\x9a\xe0\xb1\x86\xe0\xb0\xaa\xe0\xb1\x8d\xe0\xb0\xaa\xe0\xb0\xa1\xe0\xb0\xbe\xe0\xb0\xa8\xe0\xb0\xbf\xe0\xb0\x95\xe0\xb0\xbf \xe0\xb0\xaa\xe0\xb1\x8d\xe0\xb0\xb0\xe0\xb0\xaf\xe0\xb0\xa4\xe0\xb1\x8d\xe0\xb0\xa8\xe0\xb0\xbf\xe0\xb0\x82\xe0\xb0\x9a\xe0\xb0\x82\xe0\xb0\xa1\xe0\xb0\xbf...'.decode('utf-8'))
         return "None"
     return command
-
 def runVoiceAssistant() :
     userComm = ""
     action = ['open','close','search']
@@ -32,19 +28,15 @@ def runVoiceAssistant() :
     browsers = ['broswer','edge','google.com','google','firefox','open browser']
     music = ['music','song','songs','play music','play','music player']
     while userComm != 'exit' :
-        userComm = td.getenglish(input())
+        userComm = td.getenglish(userInput())
         rs.encrypt(''.join(userComm))
         if "canvas" in userComm :
             process = os.startfile(r"C:\Users\user\OneDrive\Desktop\Final Project\Python files\canvasThread.py")
-            while(process!=0) :
-                time.sleep(15)
+            while(process) :
+                time.sleep(5)
         if any(browsers in userComm for browsers in browsers) :
             ffbrow = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
             webbrowser.register('firefox', None,webbrowser.BackgroundBrowser(ffbrow))
-            
-            # website = [w for w in userComm.split() if w.endswith('.కామ్') or w.endswith('ఇన్')]
-            # website = ''.join(website)
-            
             websiteRegex = re.compile("[a-zA-Z0-9@:%._\\+~#?&//=]" + "{2,256}\\.[a-z]" +"{2,6}\\b([-a-zA-Z0-9@:%" +"._\\+~#?&//=]*)")
             site = websiteRegex.search(userComm)
             if site == None :
@@ -64,21 +56,18 @@ def runVoiceAssistant() :
         elif ('weather update' in userComm) or ('weather'  in userComm):
             cities=['delhi','hyderabad','kolkata','mumbai','chennai','sydney','california','unitedKingdom','srilanka','egypt']
             city ='hyderabad'
-            cm=list(userComm.split(' '))
-            # print(cm)
             cm  = list(userComm.split())
-            print(cm)
             for i in cities :
                 if i in userComm :
                     city = i 
                     print(city)
-            print(city+' వా తా వ ర ణం ఇ లా ఉం ది')
+            print(city+ b'\xe0\xb0\xb5\xe0\xb0\xbe \xe0\xb0\xa4\xe0\xb0\xbe \xe0\xb0\xb5 \xe0\xb0\xb0 \xe0\xb0\xa3\xe0\xb0\x82 \xe0\xb0\x87 \xe0\xb0\xb2\xe0\xb0\xbe \xe0\xb0\x89\xe0\xb0\x82 \xe0\xb0\xa6\xe0\xb0\xbf'.decode('utf-8'))
             res = requests.get('http://api.openweathermap.org/data/2.5/weather?'+"q="+city+'&appid=936a4d1ae329696c9921840fde868f19')
             result = res.json()
             farht = result['main']['temp']
             cels = int(int(farht-32)*0.55)
-            print("ఉ ష్ణో గ్ర త : "+ str(result['main']['temp']) +" K")
-            print("తే మ    : "+ str(result['main']['humidity']) +" g/Kg") 
+            print( b'\xe0\xb0\x89 \xe0\xb0\xb7\xe0\xb1\x8d\xe0\xb0\xa3\xe0\xb1\x8b \xe0\xb0\x97\xe0\xb1\x8d\xe0\xb0\xb0 \xe0\xb0\xa4 : '.decode('utf-8'), str(result['main']['temp']) +" F")
+            print(b'\xe0\xb0\xa4\xe0\xb1\x87 \xe0\xb0\xae    : '.decode('utf-8')+ str(result['main']['humidity']) +" g/Kg") 
         elif any(music in userComm for music in music) :
             if ('open' in userComm) or ('play' in userComm) :
                 path = "C:\\Users\\user\\OneDrive\\Desktop\\Final Project\\Songs\\"
@@ -98,23 +87,24 @@ def runVoiceAssistant() :
             if 'open' in userComm or 'show' in userComm:
                 path = 'C:\\Users\\user\\OneDrive\\Desktop\\Final Project\\Images\\'
                 image = os.listdir(path)
-                pic = random.choice(image)
+                pic = random.choice(image) 
+                pic = random.choice(image) 
                 os.startfile(path+pic)
             elif 'close' in userComm or 'exit' in userComm:
                 os.system('taskkill /f /im Microsoft.Photos.exe')
         elif 'news' in userComm :
             res = requests.get('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=920f8ab777584d5da8999efebb4d7777')
-            news = res.json()
-            for i in (news['articles']) :
-                print('------------------------------------')
+            news = res.json() 
+            for i in (news['articles']) : 
+                print('------------------------------------')   
                 print(i['description'])
         elif 'jokes' in userComm or 'bored' in userComm or 'laugh' in userComm:
-            print('Let me get some Jokes for you')
+            print(b'\xe0\xb0\xa8\xe0\xb1\x87\xe0\xb0\xa8\xe0\xb1\x81 \xe0\xb0\xae\xe0\xb1\x80 \xe0\xb0\x95\xe0\xb1\x8b\xe0\xb0\xb8\xe0\xb0\x82 \xe0\xb0\x95\xe0\xb1\x8a\xe0\xb0\xa8\xe0\xb1\x8d\xe0\xb0\xa8\xe0\xb0\xbf \xe0\xb0\x9c\xe0\xb1\x8b\xe0\xb0\x95\xe0\xb1\x81\xe0\xb0\xb2\xe0\xb1\x81 \xe0\xb0\xb5\xe0\xb1\x86\xe0\xb0\xa4\xe0\xb1\x81\xe0\xb0\x95\xe0\xb1\x81\xe0\xb0\xa4\xe0\xb1\x81\xe0\xb0\xa8\xe0\xb1\x8d\xe0\xb0\xa8\xe0\xb0\xbe\xe0\xb0\xa8\xe0\xb1\x81'.decode('utf-8'))
             time.sleep(0.5)
             res = requests.get('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,sexist&type=twopart&amount=5')
             result = res.json()
             for i in (result['jokes']) :
-                print('setup   : ',i['setup'])
+                print('setup   : ',i['setup']) 
                 print('delivery: ',i['delivery'])
                 print('-------------------------------------')
         elif 'exit' in userComm :
@@ -122,6 +112,4 @@ def runVoiceAssistant() :
 if __name__ == '__main__' :
     thread1 = threading.Thread(target=runVoiceAssistant())
     thread1.start()
-    
-
     runVoiceAssistant()
